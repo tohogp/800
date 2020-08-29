32ca02af86e0a681b1978f06af4c91c347013acd
KeiKawano <keikaw12@gmail.com>
2020-08-29 23:07:49 +0900

    add config for firebase

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,7 +1,12 @@
 import firebase from 'firebase'
 
 const config = {
-    projectId: process.env.FIREBASE_PROJECT_ID
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGEING_SENDER_ID
 }
 
 if (!firebase.apps.length) {
